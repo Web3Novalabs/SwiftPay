0f3ba25ce57c9b4170778e84128f687d135fcb5e
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2025-08-18 21:30:12 +0100

    feat: update address

--- frontend/src/app/components/CreateGroupForm.tsx
+++ frontend/src/app/components/CreateGroupForm.tsx
@@ -31,8 +31,11 @@ import { useCallback } from "react";
 
 export const OLD_CONTRACT_ADDRESS =
   "0x057500f7e000dafe7350eee771b791a4d885db920539e741f96410e42809a68d";
-  export const SWIFTPAY_CONTRACT_ADDRESS =
+  export const SEMI_OLD_SWIFTPAY_CONTRACT_ADDRESS =
     "0x02cc3107900daff156c0888eccbcd901500f9bf440ab694e1eecc14f4641d1dc";
+  
+    export const SWIFTPAY_CONTRACT_ADDRESS =
+      "0x0319c0feb56d2352681e58efc8aefa12efe0389b020efdcf7b822971a999f8c2";
   
 
 export const STRK_SEPOLIA =
