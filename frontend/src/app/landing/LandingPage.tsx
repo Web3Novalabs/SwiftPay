1b6ce15b246dafacbcbfae0f226399400ef006c5
martinvibes <martinmachiebe21@gmail>
2025-08-19 17:16:28 +0100

    feat: impl landing page

---
+++ frontend/src/app/landing/LandingPage.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import logo from "../../../public/paymeshLogo.svg";
+import bravos from "../../../public/braavos_icon.jpeg.svg";
+import argent from "../../../public/Argent.svg";
+import flow1 from "../../../public/Group (1).svg";
+import flow2 from "../../../public/Group (2).svg";
+import flow3 from "../../../public/Group 3.svg";
+import paymesh_landing_bg from "../../../public/bg_Landing.png";
+
+const PaymeshFlow = [
+  {
+    title: "Connect Wallet",
+    description: "Choose between argent or braavos",
+    image: flow1,
+  },
+  {
+    title: "Create a group",
+    description: "Create a new group to pool funds",
+    image: flow2,
+  },
+  {
+    title: "Share funding address",
+    description: "Use address to receive tokens ",
+    image: flow3,
+  },
+];
+
+const NavItems = [
+  {
+    name: "Home",
+    link: "/",
+  },
+
+  {
+    name: "Launch App",
+    link: "/launch-app",
+  },
+  {
+    name: "Waitlist",
+    link: "/waitlist",
+  },
+];
+
+const LandingPage = () => {
+  const [active, setActive] = useState("Home");
+
+  return (
+    <div className="relative min-h-screen">
+      <div
+        className="bg-cover bg-center bg-no-repeat w-full h-full absolute top-0 left-0 -z-10"
+        style={{
+          backgroundImage: `url(${paymesh_landing_bg.src})`,
+        }}
+      />
+
+      <div className="flex justify-center items-center pt-5 z-30">
+        <Image src={logo} alt="logo" className="" />
+      </div>
+
+      <div className="flex items-center justify-center flex-col mx-auto mt-12 sm:mt-16 md:mt-24 text-center px-4 sm:px-6 md:px-0">
+        <div className="bg-[#FFFFFF0D] w-fit mx-auto py-2.5 px-5 rounded-3xl text-sm">
+          <p>Trustworthy group funding on StarkNet</p>
+        </div>
+
+        <div className="">
+          <h1 className="bg-gradient-to-r from-[#DFDFE0] to-[#282B31] bg-clip-text py-6 text-transparent font-semibold text-[24px] sm:text-[30px] md:text-[50px]">
+            Transparent group funding on Starknet
+          </h1>
+
+          <p className="text-center text-[#8398AD] text-sm sm:text-base md:text-lg font-medium">
+            Create a funding group, share a single deposit address, and
+            automatically distribute{" "}
+            <span className="hidden sm:inline">
+              <br />
+            </span>{" "}
+            funds to members.
+          </p>
+        </div>
+
+        <div className="flex flex-col sm:flex-row items-center justify-center gap-3 sm:gap-4 mt-8 sm:mt-10">
+          <div className="border-gradient hover:scale-102 transition-all cursor-pointer">
+            <Image src={bravos} alt="bravos" width={28} height={28} />
+            <p>Connect Braavos</p>
+          </div>
+
+          <div className="border-gradient hover:scale-102 transition-all cursor-pointer">
+            <Image src={argent} alt="argent" width={28} height={28} />
+            <p>Connect ArgentX</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-[95%] sm:w-[90%] md:w-[80%] mx-auto items-center justify-center gap-4 sm:gap-5 mt-16 sm:mt-18 md:mt-20 pb-20 px-6 md:pb-0">
+        {PaymeshFlow.map((flow, i) => (
+          <div
+            key={i}
+            className="flex flex-col items-center gap-2 border-gradient-flow p-4 sm:p-6 rounded-3xl hover:scale-105 transition-all"
+          >
+            <Image
+              src={flow.image}
+              alt={flow.title}
+              width={100}
+              height={100}
+              className="mb-4 sm:mb-7 w-16 h-16 sm:w-20 sm:h-20 md:w-[100px] md:h-[100px]"
+            />
+            <h3 className="text-sm sm:text-base">{flow.title}</h3>
+            <p className="text-center text-[#8398AD] -mt-2 text-xs sm:text-sm font-medium">
+              {flow.description}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      {/* <div className="flex absolute bottom-0 left-0 w-full bg-[#0A1223] items-center justify-center gap-5 mt-20">
+        <ul className="flex items-center gap-5 p-3">
+          <li className="cursor-pointer text-white">Home</li>
+          <li className="border-gradient !py-2.5 !px-7 cursor-pointer">
+            Launch App
+          </li>
+          <li className="cursor-pointer text-white">Waitlist</li>
+        </ul>
+      </div> */}
+
+      <div className="flex fixed bottom-0 left-0 w-full bg-[#0A1223] items-center justify-center gap-2 sm:gap-3 md:gap-5 mt-20">
+        <ul className="flex items-center gap-2 sm:gap-3 md:gap-5 p-2 sm:p-3">
+          {NavItems.map((item, i) => {
+            if (item.name === "Launch App") {
+              return (
+                <li
+                  key={i}
+                  className="border-gradient !py-1.5 sm:!py-2 md:!py-2.5 !px-4 sm:!px-6 md:!px-7 cursor-pointer text-xs sm:text-sm md:text-base"
+                >
+                  {item.name}
+                </li>
+              );
+            }
+
+            return (
+              <li
+                key={i}
+                className={`cursor-pointer text-white !py-1 !px-2 sm:!px-3 md:!px-4 transition-all text-xs sm:text-sm md:text-base ${
+                  active === item.name ? "border-b-2 border-[#E2E2E2]" : ""
+                }`}
+                onClick={() => setActive(item.name)}
+              >
+                {item.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
