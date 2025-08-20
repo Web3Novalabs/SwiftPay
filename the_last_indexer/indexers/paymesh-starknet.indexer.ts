369ee38f822ef08cb25c5a04d21761513b745bc8
Akshola00 <akinniyishola07@gmail.com>
2025-08-20 15:18:50 +0100

    full indexer working

--- the_last_indexer/indexers/paymesh-starknet.indexer.ts
+++ the_last_indexer/indexers/paymesh-starknet.indexer.ts
@@ -1,58 +1,59 @@
 import { defineIndexer } from "apibara/indexer";
 import { useLogger } from "apibara/plugins";
 
-import { StarknetStream, getSelector } from "@apibara/starknet";
+import { StarknetStream, getSelector, FieldElement } from "@apibara/starknet";
 import type { ApibaraRuntimeConfig } from "apibara/types";
 
 export default function (runtimeConfig: ApibaraRuntimeConfig) {
   const { startingBlock, streamUrl } = runtimeConfig["paymeshStarknet"];
 
+  const TRANSFER_SELECTOR = getSelector("Transfer");
+  const GROUP_CREATED_SELECTOR = getSelector("GroupCreated");
+
+  let group_addresses: FieldElement[] = [];
+
   return defineIndexer(StarknetStream)({
     streamUrl,
     finality: "accepted",
-    startingBlock: BigInt(startingBlock),
+    startingBlock: BigInt(1624892),
     filter: {
       events: [
         {
           address: "0x02cc3107900daff156c0888eccbcd901500f9bf440ab694e1eecc14f4641d1dc",
-          keys: [getSelector("GroupCreated")],
+          keys: [GROUP_CREATED_SELECTOR],
         },
-        // {
-        //   address: "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
-        //   keys: [getSelector("Transfer")]
-        // }
+        {
+          address: "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+          keys: [TRANSFER_SELECTOR]
+        },
       ],
     },
-    debug: true,
     plugins: [],
-
-    // The transform function called for each block received from the DNA stream.
-    async transform({ block, finality }) {
+    async transform({ block }) {
       const logger = useLogger();
       const { events: blockEvents, header } = block;
-
       if (!header) return;
-      
-      // logger.log(`Processing block ${header.blockNumber}`);
 
+      if (header && header.blockNumber !== undefined) {
+        logger.info(`Processing block number: ${header.blockNumber}`);
+      }
+      
       for (const event of blockEvents) {
-        try {
-          let eventKey = event.keys[0];
-          if (eventKey === getSelector("GroupCreated")) {
-            console.log("✅ Processing GroupCreated event");
+        let eventKey = event.keys[0];
+        if (eventKey === GROUP_CREATED_SELECTOR) {
+            logger.info("✅ Processing GroupCreated event");
             const groupAddress = event.keys[1];
-            console.log("🔍 Parsed GroupCreated event - group_address from keys[1]:", groupAddress);      
+            logger.info(`🔍 Parsed GroupCreated event - group_address from keys[1]: ${groupAddress}`);
+            group_addresses.push(groupAddress);
+            logger.info(`Current group_addresses array: ${JSON.stringify(group_addresses)}`);
+        } 
+        else if (eventKey === TRANSFER_SELECTOR) {
+          const [, to, , ] = event.data;
+          if (group_addresses.includes(to)) {
+              logger.info(`Current group_addresses array when transferring: ${JSON.stringify(group_addresses)}`);
+              console.log("🎯 transaction found (to is in our contract addresses)");
+              console.log("Event Data: \n", event.data);
           }
-          // else if (eventKey === getSelector("Transfer")) {
-          //   // console.log("✅ A transfer occured on the starknet blockchain");
-          //   const address_to = event.keys[1];
-          //   // if (address_to == "0x06cbcd340c30ffa75dc7d8b716aa0f40d23a0d6eacd59a5c3c9a12e7371a6b9b") {
-          //     console.log(`🎯 ERC20 Transfer to address: ${address_to}`);
-          //     console.log("moeny has been sent tot he child address");
-          //   // }
-          // }
-        } catch (error) {
-          console.error("❌ Error processing event:", error);
         }
       }
 
