bbaefd50516f3d220f353e65b4d20b362895b864
Akshola00 <akinniyishola07@gmail.com>
2025-08-24 15:51:55 +0100

    change to mainnet

--- the_last_indexer/apibara.config.ts
+++ the_last_indexer/apibara.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
   runtimeConfig: {
     paymeshStarknet: {
       startingBlock: 1679531,
-      streamUrl: "https://sepolia.starknet.a5a.ch",
+      streamUrl: "https://mainnet.starknet.a5a.ch",
       contractAddress:
-        "0x03eb5cc3d473d59331c48096cafa360d52b49fcd6a08b14a6811223c773a2d73",
+        "",
     },
   },
 });
